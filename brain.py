2e90573412f4c8ad3827cd0c0c99b41eb1de589e
Armaan4123 <38943212+Armaan4123@users.noreply.github.com>
2018-05-03 19:53:47 +0530

    brainNEO.by

--- brain.py
+++ brain.py
@@ -1,20 +1,27 @@
-greet = {"hola","hey","hello","hi","hola neo","hey neo","hello neo","hi neo"}
+greetI = {"hola","hey","hello","hi","hola neo","hey neo","hello neo","hi neo"}
 quizI = {"quiz","quiz me","quiz me neo"}
-print ("you can start a quiz by saying 'quiz me neo'")
-print ("you can greet neo by saying 'hi'")
-I1=str(input("                            hello I am Neo How can I help you?               "))
+singI = {"sing","sing a song"}
+I1 = str(input("hello how may I help you? "))
 
-
-def greeting():
+def greet():
 	print("hello ,I am NEO ... Nice to meet you..!!")
 
+def sing():
+	print("I don't actually have a mouth!!!")
+
 def quiz():
-	print("what is the biggest two digit number that is divisible by 2?")
-    I2 = str(input("                            "))
-    if I2 == '98':
-    	print("good !! , correct Answer")
-    if I1 in greet :
-		greeting()
-	elif I2 in quizI :
-		quiz()
+	I2 = str(input("what is the biggest two digit number that is divisible by 2?"))
+	if I2 == '98':
+		print("good,correct answer!!")
+	else :
+		print("hmmm!!! try again later")
+
+if I1 in greetI :
+	greet()
+elif I1 in quizI:
+	quiz()
+elif I1 in singI:
+	sing()
+else:
+	print ("sometimes I don't understand you!!!")
 
